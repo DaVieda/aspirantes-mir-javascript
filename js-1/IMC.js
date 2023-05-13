0e8699c3d3375ab905005c91a6b5c7e03794e7fc
Davieda <daniel.vieda@gmail.com>
2023-05-12 22:46:46 -0500

    Solución a los ejercicios de js

---
+++ js-1/IMC.js
@@ -0,0 +1,5 @@
+function bmi(w, h){
+  return w/(h**2)
+}
+console.log("Peso:80\nTalla:1.75")
+console.log("Tu Indice de Masa Corporal es: " + bmi(80, 1.75))
---
+++ js-1/hello.js
@@ -0,0 +1,8 @@
+// escribe la función hola acá
+function fhola(nombre) {
+  console.log("Hola " + nombre +"!")
+}
+// código de prueba
+console.log(fhola("Pedro")) // "Hola Pedro!"
+console.log(fhola("Juan")) // "Hola Juan!"
+console.log(fhola("")) // "Hola !"
