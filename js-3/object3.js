186deed07221a385de3045f8a34de18294c48e1c
Davieda <daniel.vieda@gmail.com>
2023-05-17 18:06:38 -0500

    Solucion final Bonus 2

---
+++ js-3/object3.js
@@ -0,0 +1,21 @@
+/* Bonus II:
+En un archivo llamado object3.js Crear una variable llamada receta e inicialízalo con
+un objeto literal vacío. */
+
+let receta = {};
+receta.nombre = "Sandwich";
+receta.ingredientes = [];
+console.log(receta);
+
+receta.ingredientes[0] = {nombre: "Pan", cantidad: 2};
+receta.ingredientes[1] = {nombre: "Queso", cantidad: 1};
+console.log(receta);
+console.log("Primer ingrediente; " + receta.ingredientes[0]["nombre"]);
+
+// Sumar cantidades:
+let longitud = receta.ingredientes.length;
+let total = 0;
+for (let i=0; i<longitud; i++) {
+  total += receta.ingredientes[i]["cantidad"];
+}
+console.log("Total de ingredientes = " + total);
